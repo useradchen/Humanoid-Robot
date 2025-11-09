9d92310e2df06e532004d59f2ad48203a9c0265f
Eloisa <128975944+useradchen@users.noreply.github.com>
2025-11-09 22:38:44 +0800

    Add files via upload

---
+++ walking_new_715_0813/walking_new_715_0813.ino
@@ -0,0 +1,119 @@
+#include <DynamixelWorkbench.h>
+#include <math.h>
+#include "robotController.h"
+
+void setup() {
+    delay(200);
+    Serial.begin(115200);
+    delay(200);
+
+    Serial.println("=== Program Start ===");
+    Serial.println("Enter x y z (ex 5 5 5) or 'q' to stop.");
+    delay(200);
+
+    if (!initializePosition()) {
+        Serial.println("Initial position setting failed!");
+        // while(1);
+    }
+    while(Serial.available()){
+    Serial.read();
+  }
+}
+
+String inputString = "";         
+bool stringComplete = false;     
+bool isWalking = false;         
+float inputX = 0, inputY = 0, inputZ = 0, inputTheta = 0.0;
+
+void loop() {
+  // moveToPos(5, 5, 5);
+  // moveToPos(5, -5, 5);
+  // delay(10000);
+    // // 讀取串口輸入
+    while (Serial.available()) {
+        char inChar = (char)Serial.read();
+        inputString += inChar;
+        if (inChar == '\n') {
+            stringComplete = true;
+        }
+    }
+  
+    if (stringComplete) {
+        inputString.trim();
+
+        // 'q' for stop
+        if (inputString.equalsIgnoreCase("q")) {
+            Serial.println("stop");
+            walking(inputX, inputY, inputZ, inputTheta, true);
+            isWalking = false;
+            // currentStep = 0;
+            inputString = "";
+            stringComplete = false;
+            while (Serial.available()) {
+                Serial.read();
+            }
+            Serial.println("\nerror Enter x y z (ex 5 5 5) or 'q' to stop.");
+            return;
+        }
+
+        // 解析 x,y,z
+        // int comma1 = inputString.indexOf(',');
+        // if (comma1 != -1) {
+        //     int comma2 = inputString.indexOf(',', comma1 + 1);
+        //     if (comma2 != -1) {
+        //         String x_str = inputString.substring(0, comma1);
+        //         String y_str = inputString.substring(comma1 + 1, comma2);
+        //         String z_str = inputString.substring(comma2 + 1);
+
+        //         // string -> float
+        //         inputX = x_str.toFloat();
+        //         inputY = y_str.toFloat();
+        //         inputZ = z_str.toFloat();
+
+          int comma1 = inputString.indexOf(',');
+          if (comma1 != -1) {
+            int comma2 = inputString.indexOf(',', comma1 + 1);
+            if (comma2 != -1) {
+              int comma3 = inputString.indexOf(',', comma2 + 1);
+              if (comma3 != -1) {
+                String x_str = inputString.substring(0, comma1);
+                String y_str = inputString.substring(comma1 + 1, comma2);
+                String z_str = inputString.substring(comma2 + 1, comma3);
+                String theta_str = inputString.substring(comma3 + 1);
+
+                inputX = x_str.toFloat();
+                inputY = y_str.toFloat();
+                inputZ = z_str.toFloat();
+                inputTheta = theta_str.toFloat();
+                
+                Serial.print("x=");
+                Serial.print(inputX);
+                Serial.print(", y=");
+                Serial.print(inputY);
+                Serial.print(", z=");
+                Serial.println(inputZ);
+                Serial.print(", theta=");
+                Serial.println(inputTheta);
+
+                // 開始或更新步行
+                isWalking = true;
+                // currentStep = 1; // 從第一步開始
+            } else {
+                Serial.println("\nerror Enter x y z (ex 5 5 5) or 'q' to stop.");
+            }
+          }
+        } else {
+            Serial.println("\nerror Enter x y z (ex 5 5 5) or 'q' to stop.");
+        }
+        // 重置輸入
+        inputString = "";
+        stringComplete = false;
+        Serial.println("\nerror Enter x y z (ex 5 5 5) or 'q' to stop.");
+
+    }
+    // moveToPos(inputX, inputY, inputZ); // for 20,20,20
+    // 執行步行邏輯
+    if (isWalking) {
+        walking(inputX, inputY, inputZ, inputTheta, false);
+    }
+}
